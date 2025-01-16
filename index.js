ab1581b5069045408576cdd1f07b5f32680890a1
tm-fuad05 <tm.fuad05@gmail.com>
2025-01-16 15:13:15 +0600

    connect mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dmsil.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Database & Collection
+    const database = client.db("FitRackDB");
+    const userCollection = database.collection("users");
+
+    userCollection.insertOne({ name: "fuad", age: 19 });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
